9fe86ab555ebade7641e34c5b9848b41043845b0
Segaud Kevin <segaud.kevin@gmail.com>
2015-09-18 17:42:04 +0200

    sort function by name

--- lib/reed_solomon.dart
+++ lib/reed_solomon.dart
@@ -2,15 +2,26 @@ library reed_solomon;
 
 import "package:galois_field/galois_field.dart";
 
-/**
- * Computes the generator polynomial for a given number of error correction symbols
- */
-List<int> _rsGeneratorPolynomial(int nsym) {
-  List<int> g = [1];
-  for (int i = 0; i < nsym; i++) {
-    g = gfPolynomialMultiply(g, [1, GF_EXP[i]]);
+List<int> rsCorrectMessage(List<int> message_in, int nsym) {
+  List<int> message_out = new List.from(message_in);
+  List<int> erase_pos = [];
+  for (int i = 0; i < message_out.length; i++) {
+    if (message_out[i] < 0) {
+      message_out[i] = 0;
+      erase_pos.add(i);
+    }
   }
-  return g;
+  if (erase_pos.length > nsym) return null;
+  List<int> synd = _rsCalculateSyndrome(message_out, nsym);
+  if (_max(synd) == 0) return message_out;
+  List<int> fsynd = _rsForneySyndrome(synd, erase_pos, message_out.length);
+  List<int> err_polynomial = _rsGeneratorErrorPolynomial(fsynd);
+  List<int> err_pos = _rsFindErrors(err_polynomial, message_out.length);
+  if (err_pos == null) return null;
+  message_out = _rsCorrectErrata(message_out, synd, erase_pos..addAll(err_pos));
+  synd = _rsCalculateSyndrome(message_out, nsym);
+  if (_max(synd) > 0) return null;
+  return message_out;
 }
 
 /**
@@ -33,6 +44,17 @@ List<int> rsEncodeMessage(List<int> message_in, int nsym) {
   return []..addAll(message_out);
 }
 
+int _max(List<int> list) {
+  int neg = -999999999999999;
+  int r = null;
+  list.forEach((int value) {
+    if (value > neg) {
+      r = value;
+    }
+  });
+  return r;
+}
+
 /**
  * Calculate the syndromes
  */
@@ -42,31 +64,6 @@ List<int> _rsCalculateSyndrome(List<int> msg, int nsym) {
     synd[i] = gfPolynomialEval(msg, GF_EXP[i]);
   }
   return synd;
-}
-
-/**
- * Compute the erasures/errors/errata locator polynomial from the erasures/errors/errata positions
- * (the positions must be relative to the x coefficient, eg: "hello worldxxxxxxxxx" is tampered to "h_ll_ worldxxxxxxxxx"
- * with xxxxxxxxx being the ecc of length n-k=9, here the string positions are [1, 4], but the coefficients are reversed
- * since the ecc characters are placed as the first coefficients of the polynomial, thus the coefficients of the
- * erased characters are n-1 - [1, 4] = [18, 15] = erasures_loc to be specified as an argument.
- */
-List<int> _rsFindErrataLocator(List<int> e_pos, {int x: null}) {
-  List<int> e_loc = [1];
-  for (x in e_pos) {
-    e_loc = gfPolynomialMultiply(e_loc, gfPolynomialAdd([1], [GF_EXP[x], 0]));
-  }
-  return e_loc;
-}
-
-/**
- * Compute the error (or erasures if you supply sigma=erasures locator polynomial, or errata) evaluator polynomial Omega
- * from the syndrome and the error/erasures/errata locator Sigma.
- */
-List<int> _rsFindErrorEvaluator(List<int> synd, List<int> err_loc, int nsym) {
-  List<int> remainder = gfPolynomialDivide(gfPolynomialMultiply(synd, err_loc),
-      [1]..addAll(new List.filled(nsym + 1, 0)));
-  return remainder;
 }
 
 /**
@@ -99,33 +96,28 @@ List<int> _rsCorrectErrata(List<int> message, List<int> synd, List<int> pos) {
 }
 
 /**
- * Find error locator polynomial with Berlekamp-Massey algorithm
+ * Compute the erasures/errors/errata locator polynomial from the erasures/errors/errata positions
+ * (the positions must be relative to the x coefficient, eg: "hello worldxxxxxxxxx" is tampered to "h_ll_ worldxxxxxxxxx"
+ * with xxxxxxxxx being the ecc of length n-k=9, here the string positions are [1, 4], but the coefficients are reversed
+ * since the ecc characters are placed as the first coefficients of the polynomial, thus the coefficients of the
+ * erased characters are n-1 - [1, 4] = [18, 15] = erasures_loc to be specified as an argument.
  */
-List<int> _rsGeneratorErrorPolynomial(List<int> synd) {
-  List<int> err_loc = [1];
-  List<int> old_loc = [1];
+List<int> _rsFindErrataLocator(List<int> e_pos, {int x: null}) {
+  List<int> e_loc = [1];
+  for (x in e_pos) {
+    e_loc = gfPolynomialMultiply(e_loc, gfPolynomialAdd([1], [GF_EXP[x], 0]));
+  }
+  return e_loc;
+}
 
-  for (int i = 0; i < synd.length; i++) {
-    old_loc.add(0);
-    int delta = synd[i];
-    for (int j = 1; j < err_loc.length; j++) {
-      delta ^= gfMultiply(err_loc[err_loc.length - 1 - j], synd[i - j]);
-    }
-    if (delta != 0) {
-      if (old_loc.length > err_loc.length) {
-        List<int> new_loc = gfPolynomialScale(old_loc, delta);
-        old_loc = gfPolynomialScale(err_loc, gfInverse(delta));
-        err_loc = new_loc;
-      }
-      err_loc = gfPolynomialAdd(err_loc, gfPolynomialScale(old_loc, delta));
-    }
-  }
-  err_loc.removeWhere((int value) => value == 0);
-  int errs = err_loc.length - 1;
-  if (errs * 2 > synd.length) {
-    return null;
-  }
-  return err_loc;
+/**
+ * Compute the error (or erasures if you supply sigma=erasures locator polynomial, or errata) evaluator polynomial Omega
+ * from the syndrome and the error/erasures/errata locator Sigma.
+ */
+List<int> _rsFindErrorEvaluator(List<int> synd, List<int> err_loc, int nsym) {
+  List<int> remainder = gfPolynomialDivide(gfPolynomialMultiply(synd, err_loc),
+      [1]..addAll(new List.filled(nsym + 1, 0)));
+  return remainder;
 }
 
 /**
@@ -161,35 +153,43 @@ List<int> _rsForneySyndrome(List<int> synd, List<int> pos, int nmess) {
   return fsynd;
 }
 
-List<int> rsCorrectMessage(List<int> message_in, int nsym) {
-  List<int> message_out = new List.from(message_in);
-  List<int> erase_pos = [];
-  for (int i = 0; i < message_out.length; i++) {
-    if (message_out[i] < 0) {
-      message_out[i] = 0;
-      erase_pos.add(i);
+/**
+ * Find error locator polynomial with Berlekamp-Massey algorithm
+ */
+List<int> _rsGeneratorErrorPolynomial(List<int> synd) {
+  List<int> err_loc = [1];
+  List<int> old_loc = [1];
+
+  for (int i = 0; i < synd.length; i++) {
+    old_loc.add(0);
+    int delta = synd[i];
+    for (int j = 1; j < err_loc.length; j++) {
+      delta ^= gfMultiply(err_loc[err_loc.length - 1 - j], synd[i - j]);
+    }
+    if (delta != 0) {
+      if (old_loc.length > err_loc.length) {
+        List<int> new_loc = gfPolynomialScale(old_loc, delta);
+        old_loc = gfPolynomialScale(err_loc, gfInverse(delta));
+        err_loc = new_loc;
+      }
+      err_loc = gfPolynomialAdd(err_loc, gfPolynomialScale(old_loc, delta));
     }
   }
-  if (erase_pos.length > nsym) return null;
-  List<int> synd = _rsCalculateSyndrome(message_out, nsym);
-  if (_max(synd) == 0) return message_out;
-  List<int> fsynd = _rsForneySyndrome(synd, erase_pos, message_out.length);
-  List<int> err_polynomial = _rsGeneratorErrorPolynomial(fsynd);
-  List<int> err_pos = _rsFindErrors(err_polynomial, message_out.length);
-  if (err_pos == null) return null;
-  message_out = _rsCorrectErrata(message_out, synd, erase_pos..addAll(err_pos));
-  synd = _rsCalculateSyndrome(message_out, nsym);
-  if (_max(synd) > 0) return null;
-  return message_out;
+  err_loc.removeWhere((int value) => value == 0);
+  int errs = err_loc.length - 1;
+  if (errs * 2 > synd.length) {
+    return null;
+  }
+  return err_loc;
 }
 
-int _max(List<int> list) {
-  int neg = -999999999999999;
-  int r = null;
-  list.forEach((int value) {
-    if (value > neg) {
-      r = value;
-    }
-  });
-  return r;
+/**
+ * Computes the generator polynomial for a given number of error correction symbols
+ */
+List<int> _rsGeneratorPolynomial(int nsym) {
+  List<int> g = [1];
+  for (int i = 0; i < nsym; i++) {
+    g = gfPolynomialMultiply(g, [1, GF_EXP[i]]);
+  }
+  return g;
 }
